80303839a7ac931636e3d0bbd790b30a403c682c
dianacovacii <diana.covaci@mail.mcgill.ca>
2025-11-15 13:52:35 -0500

    data collected and characters chosen for The Office

---
+++ collect_transcripts.py
@@ -0,0 +1,35 @@
+from kaggle.api.kaggle_api_extended import KaggleApi
+import os
+import pandas as pd
+
+
+def get_kaggle_dataset(): 
+  file_path = 'data/The-Office-Lines-V4.csv'
+  if not os.path.exists(file_path): 
+    api = KaggleApi()
+    api.authenticate()
+
+    data_dir = 'data'
+    os.makedirs(data_dir, exist_ok=True)
+
+    dataset = "nasirkhalid24/the-office-us-complete-dialoguetranscript"
+    api.dataset_download_files(dataset, path=file_path, unzip=True)
+
+
+def get_lines_of_interest(all_lines): 
+  characters_of_interest = ['Jan', 'Oscar', 'Angela', 'Toby']
+  lines_of_interest = all_lines[all_lines['speaker'].isin(characters_of_interest)]
+  return lines_of_interest
+
+
+def remove_short_lines(lines): 
+  long_lines = lines[lines['line'].str.split().str.len() > 2]
+  return long_lines
+
+
+if __name__ == '__main__': 
+  get_kaggle_dataset()
+  all_lines = pd.read_csv('data/The-Office-Lines-V4.csv')
+  character_lines = get_lines_of_interest(all_lines)
+  character_lines = remove_short_lines(character_lines)
+  character_lines.to_csv('data/character_lines', index=False)
---
+++ notes.md
@@ -0,0 +1,6 @@
+Diana -- Collecting transcripts and finding character lines, Nov 15
+
+- Friends side-characters do not have enough lines
+- pivoted to The Office as the show of choice
+- downloaded dataset of lines from kaggle which I then tailored to our project needs
+
